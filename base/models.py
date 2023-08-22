33c8be8fe7917f34b9329f1e16c66f29684a852e
Min Hein Zaw <minheinzaw@gmail.com>
2023-08-22 08:29:18 +0630

    Add slug

--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from autoslug import AutoSlugField
 from django.utils.crypto import get_random_string
+from django.utils.text import slugify
 
 # Create your models here.
 def unique_slugify(instance, slug):
@@ -16,7 +17,16 @@ class Product(models.Model):
     image = models.ImageField(upload_to='photos/', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    slug = AutoSlugField(populate_from='name', unique=True, null=True, blank=True, editable=False)
+    slug = models.SlugField(max_length=255, null=False,blank=True,unique=True,editable=False,)
+
+    def save(self, *args, **kwargs):
+        if not self.slug: # If the slug is not set
+            self.slug = unique_slugify(self,slugify(self.name,allow_unicode=True)) # Generate the slug from the title
+        else:
+            new_slug = unique_slugify(self,slugify(self.name,allow_unicode=True))
+            if self.slug != new_slug: # Check if the title has changed
+                self.slug = new_slug # Update the slug with the new value
+        super().save(*args, **kwargs) # Call the parent save method
 
     def __str__(self):
         return self.name
